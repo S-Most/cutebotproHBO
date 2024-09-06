2ac540c6588c27fae91725b48f4b8cc138554546
Sander van der Most <71282824+S-Most@users.noreply.github.com>
2024-09-06 14:13:03 +0200

    first push

--- main.ts
+++ main.ts
@@ -9,11 +9,17 @@ basic.pause(2000)
 basic.clearScreen()
 basic.forever(function () {
     CutebotPro.trackbitStateValue()
-    CutebotPro.trackbitStateValue()
-    if (CutebotPro.getGrayscaleSensorState(TrackbitStateType.Tracking_State_0)) {
-        CutebotPro.fullSpeedAhead()
-    } else {
-        CutebotPro.pwmCruiseControl(0, 0)
-        CutebotPro.extendMotorStop()
+    if (CutebotPro.getGrayscaleSensorState(TrackbitStateType.Tracking_State_14) || CutebotPro.getGrayscaleSensorState(TrackbitStateType.Tracking_State_12)) {
+        // Hard right
+        CutebotPro.pwmCruiseControl(50, 20)
+    } else if (CutebotPro.getGrayscaleSensorState(TrackbitStateType.Tracking_State_11) || CutebotPro.getGrayscaleSensorState(TrackbitStateType.Tracking_State_8)) {
+        // hard left
+        CutebotPro.pwmCruiseControl(20, 50)
+    } else if (CutebotPro.getGrayscaleSensorState(TrackbitStateType.Tracking_State_2) || false) {
+        // slow right
+        CutebotPro.pwmCruiseControl(50, 40)
+    } else if (CutebotPro.getGrayscaleSensorState(TrackbitStateType.Tracking_State_3) || false) {
+        // slow left
+        CutebotPro.pwmCruiseControl(40, 50)
     }
 })
